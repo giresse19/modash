4e9105eb42f4d39925d82266789ee4c5893478da
giresse <ajanohpelkins18@gmail.com>
2020-05-02 02:37:38 +0300

    Add type definitions for node.

--- server/app/helper/getImageRequest.ts
+++ server/app/helper/getImageRequest.ts
@@ -2,6 +2,9 @@ const vision = require("@google-cloud/vision");
 const IgConfig = require("../../config/config.ts");
 const axios = require("axios");
 
+// Instantiates a client
+const client = new vision.ImageAnnotatorClient();
+
 const getProfile = async (name: string) => {
   try {
     const result = await axios.get(
@@ -18,9 +21,6 @@ const getProfile = async (name: string) => {
 // However,response from such request can only be stored in google cloud storage.
 // Hence, for this task, opted to go with single request per media item.
 const getImageInsights = async (array: any) => {
-  // Creates a client
-  const client = new vision.ImageAnnotatorClient();
-
   const newArr = [...array];
   const obj = {
     labels: [{ description: String, score: String }],
@@ -30,21 +30,68 @@ const getImageInsights = async (array: any) => {
   for (let el of newArr) {
     let [result] = await client.labelDetection(el.media_url);
     let [logo] = await client.logoDetection(el.media_url);
-    result.labelAnnotations.forEach((label: { score: any; description: any; }) =>
-      obj.labels.push({        
+
+    result.labelAnnotations.forEach((label: { score: any; description: any }) =>
+      obj.labels.push({
         score: label.score,
         description: label.description,
       })
     );
-    logo.logoAnnotations.forEach((logo: { description: any; score: any; }) => obj.brands.push({
-      description: logo.description,
-      score: logo.score,
-    }));
+    
+    logo.logoAnnotations.forEach((logo: { description: any; score: any }) =>
+      obj.brands.push({
+        description: logo.description,
+        score: logo.score,
+      })
+    );
   }
   return obj;
+};
+
+const getBatchImage = async () => {
+  const features = [{ type: "LABEL_DETECTION" }];
+
+  const inputImageUri: string = "";
+  const outputUri: string = "";
+
+  const imageRequest = {
+    image: {
+      source: {
+        imageUri: inputImageUri,
+      },
+    },
+    features: features,
+  };
+
+  // Set where to store the results for the images that will be annotated.
+  const outputConfig = {
+    gcsDestination: {
+      uri: outputUri,
+    },
+    batchSize: 2, // The max number of responses to output in each JSON file
+  };
+
+  // Add each image request object to the batch request and add the output config.
+  const request = {
+    requests: [
+      imageRequest, // add additional request objects here
+    ],
+    outputConfig,
+  };
+
+  // Make the asynchronous batch request.
+  const [operation] = await client.asyncBatchAnnotateImages(request);
+
+  // Wait for the operation to complete
+  const [filesResponse] = await operation.promise();
+
+  // The output is written to GCS with the provided output_uri as prefix
+  const destinationUri = filesResponse.outputConfig.gcsDestination.uri;
+  console.log(`Output written to GCS with prefix: ${destinationUri}`);
 };
 
 module.exports = {
   getProfile,
   getImageInsights,
+  getBatchImage,
 };
