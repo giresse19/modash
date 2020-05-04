314b02e56ad1f744736f2288941d22ea75c2fa83
giresse <ajanohpelkins18@gmail.com>
2020-05-05 00:34:58 +0300

    remove docker, clean-up.

--- server/server.ts
+++ server/server.ts
@@ -1,14 +1,14 @@
-
+export{}
 process.on('uncaughtException', console.error);
 
 const config = require("./config/config.ts");
 const errorHandler = require("errorhandler");
-const expressApp = require('./app');
+const app = require('./app');
 
-expressApp.listen(config.port, () =>
+app.listen(config.port, () =>
   console.log(`Server started on port ${config.port}`)
 );
 
 if ("development" == config.env) {
-  expressApp.use(errorHandler({ dumpExceptions: true, showStack: true }));
+  app.use(errorHandler({ dumpExceptions: true, showStack: true }));
 }
