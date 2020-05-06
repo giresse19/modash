c22fd01f9cf7e602da828e8357fcd7cc0c51c862
giresse <ajanohpelkins18@gmail.com>
2020-05-06 19:00:04 +0300

    clean-up.

--- server/server.ts
+++ server/server.ts
@@ -1,8 +1,9 @@
+const app = require('./app');
 process.on('uncaughtException', console.error);
 
 const config = require("./config/config.ts");
 const errorHandler = require("errorhandler");
-const app = require('./app');
+
 
 app.listen(config.port, () =>
   console.log(`Server started on port ${config.port}`)
